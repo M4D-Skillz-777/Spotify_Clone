a5060bcc356e9801ff8b270b76fcee295dce689e
Adwaiy Randale <adflarezero3@gmail.com>
2021-09-20 22:35:53 +0530

    Spotify React clone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from './Login';
+import Dashbord from "./Dashbord";
+
+const code = new URLSearchParams(window.location.search).get('code')
+
+function App() {
+  return (
+    code ? <Dashbord code={code} /> : <Login />
+  );
+}
+
+export default App;
